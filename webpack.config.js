2eed876cc5f6895426fbf4df8f61bf3c2c271c76
Calvin Chan <calvin_chan137@outlook.com>
2017-05-05 01:43:39 -0400

    Init and configured

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+// https://www.codecademy.com/articles/react-setup-iv
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+
+var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+	template: __dirname + '/app/index.html',
+	filename: 'index.html',
+	inject: 'body'
+});
+
+module.exports = {
+	entry: __dirname + '/app/script.js',
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}
+		]
+	},
+	output: {
+		filename: 'transformed_script.js',
+		path: __dirname + '/build'
+	},
+	plugins: [HTMLWebpackPluginConfig]
+};
