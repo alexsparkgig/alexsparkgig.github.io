4737cd041b914e6c40bc39f3a7e8881a2556e769
Calvin Chan <calvin_chan137@outlook.com>
2017-05-12 02:11:22 -0400

    Added quote component

---
+++ app/components/Homepage/Quote/Quote.js
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var Quote = React.createClass({
+	render: function() {
+		return (
+			<div>
+				Quote
+				{/*<input type="" name="Name">
+				<input type="" name="">
+				<input type="" name="">*/}
+			</div>
+		)	
+	}
+});
+
+module.exports = Quote;
