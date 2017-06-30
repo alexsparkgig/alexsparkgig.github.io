23892644863a7dd3e4f759f689e372088ae673b3
Alex <alex@sparkgig.com>
2017-06-30 12:49:07 -0400

    .....

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 var HTMLWebpackPlugin = require('html-webpack-plugin');
 
 var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-	template: __dirname + '/index.html',
+	template: 'index.html',
 	filename: 'index.html',
 	inject: 'body'
 });
