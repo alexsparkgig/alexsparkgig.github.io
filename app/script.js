91a871c4a8c13eddc882340020d8a193a3a73468
Alex <alex@sparkgig.com>
2017-06-30 12:31:30 -0400

    .....

--- app/script.js
+++ app/script.js
@@ -3,8 +3,4 @@ var ReactDOM = require('react-dom');
 var App = require('./components/App');
 
 // entrypoint
-<<<<<<< HEAD
-// ReactDOM.render(<App />, document.getElementById('app'));
-=======
 ReactDOM.render(<App />, document.getElementById('app'));
->>>>>>> parent of f017ba1... new index
